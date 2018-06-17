b650465acf329df7a656c1ca7b1562c2430cd101
defuera <vdenis@zvooq.com>
2018-06-17 21:37:02 +0200

    improve list, loading states

--- lib/overview/page_overview.dart
+++ lib/overview/page_overview.dart
@@ -3,12 +3,10 @@ import 'package:flutter/material.dart';
 import 'presenter_overview.dart';
 
 class OverviewPage extends StatefulWidget {
-
   @override
   OverviewPageState createState() {
     return OverviewPageState();
   }
-
 }
 
 enum Status { LOADING, DATA, ERROR }
@@ -74,11 +72,34 @@ class OverviewPageState extends State<OverviewPage> {
     );
   }
 
-  Widget getLoadingView() => CircularProgressIndicator();
+  Widget getLoadingView() => Center(child: CircularProgressIndicator());
 
-  Widget getList() =>
-      ListView.builder(
+  Widget getList() => ListView.builder(
         itemCount: _data.length,
-        itemBuilder: (BuildContext context, int index) => Text(_data[index]),
+        itemBuilder: (BuildContext context, int index) => GestureDetector(
+                child: InkWell(
+              // When the user taps the button, show a snackbar
+              onTap: () {
+                Scaffold.of(context).showSnackBar(new SnackBar(
+                      content: new Text("Buy more ${_data[index]}"),
+                    ));
+              },
+              child: new Container(
+                padding: new EdgeInsets.all(16.0),
+                child: Text(
+                  _data[index],
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            )),
       );
 }
+
+//child = Container(
+//child: Text(
+//_data[index],
+//style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+//),
+//padding: EdgeInsets.all(16.0),
+//),
+//onTap: () => ,
--- lib/overview/presenter_overview.dart
+++ lib/overview/presenter_overview.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+
 import 'package:shiffr_wallet/app/repository_bitfinex.dart';
 import 'package:shiffr_wallet/overview/page_overview.dart';
 
 class OverviewPresenter {
-
   final _repository = BitfinexRepository();
 
   final OverviewPageState _overviewPageState;
@@ -16,9 +17,10 @@ class OverviewPresenter {
   void loadListPairs() async {
     _overviewPageState.showLoading();
     var data = await _repository.getSymbols();
+    await new Future.delayed(const Duration(minutes: 400)); //todo remove, it's only for debug
+
     print("loadListPairs: $data");
 
     _overviewPageState.showData(data);
   }
-
 }
